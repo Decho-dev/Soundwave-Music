22dacdd934136772ac3a56b646fc7f45d54ef364
Desislav Yordanov <54678293+Decho-dev@users.noreply.github.com>
2020-09-14 20:51:23 +0300

    Add files via upload

---
+++ discover.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    let newDiv = $('<div>');
+    let img = $('<img>');
+    img.appendTo(newDiv);
+    img.css({'width': '60%', 'height' : '80%', 'margin': '5% 20%'})
+    newDiv.appendTo('.full-container')
+    newDiv.css({'position' : 'absolute','top' : '80px', 'left' : '50%','transform': 'translateX(-50%)' ,'width': '70%' , 'height' : '70%', 'background-color' : 'rgba(255,0,0,0.3)', 'cursor' : 'pointer'})
+    newDiv.fadeOut()
+
+    $('.poster img').click(function() {
+        img.attr('src', $(this).attr('src'))
+        newDiv.stop().slideDown(200);
+    })
+
+    newDiv.click(function() {
+        newDiv.stop().slideUp(200)
+    })
+})
